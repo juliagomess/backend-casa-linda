b5b798357d6b047cd1d668e88235a3071ab2d627
Joao <joaozanholo2002@hotmail.com>
2022-05-20 10:25:55 -0300

    entidade movimentação

--- src/main/java/projeto/backend/entity/Movimentacao.java
+++ src/main/java/projeto/backend/entity/Movimentacao.java
@@ -2,10 +2,21 @@ package projeto.backend.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
 
 @Data
 @Entity
 public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private long id;
+    @ForeignKey
+    private Produto produto;
+    private int tipo;
+    private int quantidade;
+    private float valor_entrada;
+    private String data;
 
 }
